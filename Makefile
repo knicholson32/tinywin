14769b49a56095e34941c371980f647fc7f0bbfd
Keenan Nicholson <knicholson32@gatech.edu>
2020-05-06 13:04:50 -0400

    Added support for pip repo installs

--- Makefile
+++ Makefile
@@ -4,4 +4,18 @@ init:
 test:
 	py.test tests
 
+clean:
+	rm -rf ./build \
+	rm -rf ./dist  \
+	rm -rf ./tinywin.egg-info
+
+compile:
+	python3 setup.py bdist_wheel
+
+install-local:
+	python3 -m pip install dist/tinywin*.whl
+
+upload-pypi:
+	python3 -m twine upload dist/*
+
 .PHONY: init test
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='tinywin',
+    version='0.1.1',
+    scripts=[],
+    author="Keenan Nicholson",
+    author_email="",
+    description="A tiny Curses window management library, designed to work with CircuitPython_Curses",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/knicholson32/tinywin/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+)
